503a723bea0d5d6f4351402a771ab9157ba436a8
Wei Ji <23487320+weiji14@users.noreply.github.com>
2023-11-10 08:37:19 +1300

    Setup LightningCLI trainer script (#24)
    * :heavy_plus_sign: Add jsonargparse[signatures]
    
    Parsing of command line options, yaml/jsonnet config files and/or environment variables based on argparse! Also adding the signatures extras (which includes typeshed-client).
    
    * :seedling: Setup LightningCLI trainer script
    
    Setting up the command-line interface to run Lightning. Created a placeholder BaseDataModule and BaseLitModule to hold the data pipeline and model architecture respectively under the src/ folder. Documented in the main README.md on how to run the LightningCLI commands, and also created a src/README.md to document what the python modules in that folder.

---
+++ src/README.md
@@ -0,0 +1,16 @@
+# Clay Foundation Model Modules
+
+This folder contains several LightningDataModule and LightningModule classes.
+
+## DataModules (data pipeline)
+
+- datamodule.py - Base data pipeline to read in Earth Observation chips
+
+## LightningModule (model architecture)
+
+- model.py - Base neural network model architecture
+
+## References
+
+- https://lightning.ai/docs/pytorch/2.1.0/data/datamodule.html
+- https://lightning.ai/docs/pytorch/2.1.0/common/lightning_module.html
---
+++ trainer.py
@@ -0,0 +1,42 @@
+"""
+Command line interface to run the neural network model!
+
+From the project root directory, do:
+
+    python trainer.py fit
+
+References:
+- https://lightning.ai/docs/pytorch/2.1.0/cli/lightning_cli.html
+- https://pytorch-lightning.medium.com/introducing-lightningcli-v2-supercharge-your-training-c070d43c7dd6
+"""
+import torch
+from lightning.pytorch.cli import ArgsType, LightningCLI
+
+from src.datamodule import BaseDataModule
+from src.model import BaseLitModule
+
+
+# %%
+def cli_main(
+    save_config_callback=None,
+    seed_everything_default=42,
+    trainer_defaults: dict = {"logger": False, "precision": "bf16-mixed"},
+    args: ArgsType = None,
+):
+    """
+    Command-line inteface to run BaseLitModule with BaseDataModule.
+    """
+    cli = LightningCLI(
+        model_class=BaseLitModule,
+        datamodule_class=BaseDataModule,
+        save_config_callback=save_config_callback,
+        seed_everything_default=seed_everything_default,
+        trainer_defaults=trainer_defaults,
+        args=args,
+    )
+
+
+# %%
+if __name__ == "__main__":
+    cli_main()
+    print("Done!")
