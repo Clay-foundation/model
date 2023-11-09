1c6de6a99c71dbebaf0a5e2802a60bd12ee40b28
Wei Ji <23487320+weiji14@users.noreply.github.com>
2023-11-10 11:34:44 +1300

    Setup GitHub Actions Continuous Integration tests (#25)
    * :see_no_evil: Add .gitignore file
    
    * :construction_worker: Setup GitHub Actions Continuous Integration tests
    
    Running tests on Ubuntu-22.04 and Python 3.11 only for now. Add a parametrized test to ensure that `python trainer.py fit --print_config=skip_null` works (as well as the validate/test subcommands). Tests are ran using `python -m pytest src/tests/`.

---
+++ src/tests/test_trainer.py
@@ -0,0 +1,26 @@
+"""
+Tests for command-line interface to execute ChaBuDNet.
+
+Based on advanced usage of running LightningCLI from Python at
+https://lightning.ai/docs/pytorch/2.1.0/cli/lightning_cli_advanced_3.html#run-from-python
+"""
+import pytest
+
+from trainer import cli_main
+
+
+# %%
+@pytest.mark.parametrize("subcommand", ["fit", "validate", "test"])
+def test_cli_main(subcommand, capsys):
+    """
+    Ensure that running `python trainer.py` works with the subcommands `fit`
+    and `validate`.
+    """
+    with pytest.raises(expected_exception=SystemExit, match="0"):
+        cli_main(args=[subcommand, "--print_config=skip_null"])
+
+    captured = capsys.readouterr()
+    assert "seed_everything:" in captured.out
+    assert "trainer:" in captured.out
+    assert "model:" in captured.out
+    assert "data:" in captured.out
