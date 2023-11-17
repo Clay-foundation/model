fe1eab08d748e21b2eaaaf798bf45f4dca7b4f61
Lilly Thomas <lilly@developmentseed.org>
2023-11-16 16:20:00 -0800

    example script to run the datacube and tiler modules

---
+++ scripts/stack_tile.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append('./')
+from datacube import main
+from tile import tiler
+
+def run_stack_tile():
+    stack = main()
+    tiles = tiler(stack)
+    print("Stack: ", stack)
+    return tiles
+
+tiles = run_stack_tile()
+print("Number of tiles generated: ", len(tiles))
