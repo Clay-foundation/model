79882939247e13dc50c71dd14acad7d116be9780
Wei Ji <23487320+weiji14@users.noreply.github.com>
2023-11-24 19:19:01 +1300

    :recycle: Refactor test_model_vit to use datapipe fixture
    Decoupling the neural network model's unit test from the LightningDataModule by implementing a standalone datapipe fixture instead.

--- src/tests/test_model.py
+++ src/tests/test_model.py
@@ -5,24 +5,44 @@ Based loosely on Lightning's testing method described at
 https://github.com/Lightning-AI/lightning/blob/2.1.0/.github/CONTRIBUTING.md#how-to-add-new-tests
 """
 import lightning as L
+import pytest
+import torch
+import torchdata
+import torchdata.dataloader2
 
-from src.datamodule import BaseDataModule
 from src.model_vit import ViTLitModule
 
 
 # %%
-def test_model_vit():
+@pytest.fixture(scope="function", name="datapipe")
+def fixture_datapipe() -> torchdata.datapipes.iter.IterDataPipe:
+    """
+    A torchdata DataPipe with random data to use in the tests.
+    """
+    datapipe = torchdata.datapipes.iter.IterableWrapper(
+        iterable=[
+            torch.randn(2, 13, 256, 256).to(dtype=torch.float16),
+            torch.randn(2, 13, 256, 256).to(dtype=torch.float16),
+        ]
+    )
+    return datapipe
+
+
+# %%
+def test_model_vit(datapipe):
     """
     Run a full train, val, test and prediction loop using 1 batch.
     """
     # Get some random data
-    dataloader: L.LightningDataModule = BaseDataModule()
+    dataloader = torchdata.dataloader2.DataLoader2(datapipe=datapipe)
 
     # Initialize model
     model: L.LightningModule = ViTLitModule()
 
     # Training
-    trainer: L.Trainer = L.Trainer(accelerator="auto", devices=1, fast_dev_run=True)
+    trainer: L.Trainer = L.Trainer(
+        accelerator="auto", devices=1, precision="16-mixed", fast_dev_run=True
+    )
     trainer.fit(model=model, train_dataloaders=dataloader)
 
     # Test/Evaluation
