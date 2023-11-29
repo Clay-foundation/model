e75ad0eb978511e62d7622f2de150a4a912aacb1
Wei Ji <23487320+weiji14@users.noreply.github.com>
2023-11-29 14:40:45 +1300

    Configure model checkpointing (#55)
    * :wrench: Configure ModelCheckpoint callback
    
    Save model weights to a properly named checkpoint file like vit_epoch-09_train_loss-3250218.25.ckpt, stored in the checkpoints/ folder by default. More configuration can be done through LightningCLI, see `python trainer.py fit --trainer.callbacks.help=ModelCheckpoint`.
    
    * :zap: Add AsyncCheckpointIO plugin to trainer
    
    Enable the experimental plugin that saves checkpoint files asynchronously in a thread. See https://lightning.ai/docs/pytorch/2.1.0/api/lightning.pytorch.plugins.io.AsyncCheckpointIO.html.

--- trainer.py
+++ trainer.py
@@ -9,7 +9,9 @@ References:
 - https://lightning.ai/docs/pytorch/2.1.0/cli/lightning_cli.html
 - https://pytorch-lightning.medium.com/introducing-lightningcli-v2-supercharge-your-training-c070d43c7dd6
 """
+from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.cli import ArgsType, LightningCLI
+from lightning.pytorch.plugins.io import AsyncCheckpointIO
 
 from src.datamodule import GeoTIFFDataPipeModule
 from src.model_vit import ViTLitModule
@@ -19,7 +21,23 @@ from src.model_vit import ViTLitModule
 def cli_main(
     save_config_callback=None,
     seed_everything_default=42,
-    trainer_defaults: dict = {"logger": False, "precision": "bf16-mixed"},
+    trainer_defaults: dict = {
+        "callbacks": [
+            ModelCheckpoint(
+                # dirpath="checkpoints/",
+                auto_insert_metric_name=False,
+                filename="vit_epoch-{epoch:02d}_train_loss-{train/loss:.2f}",
+                monitor="train/loss",
+                mode="min",
+                save_last=True,
+                save_top_k=3,
+                save_weights_only=True,
+            ),
+        ],
+        "logger": False,
+        "plugins": [AsyncCheckpointIO()],
+        "precision": "bf16-mixed",
+    },
     args: ArgsType = None,
 ):
     """
