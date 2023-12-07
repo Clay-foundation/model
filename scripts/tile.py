21529d86998f9a4c3e7557ac6d012b4fba9e429c
Daniel Wiesmann <daniel@wiesmann.pt>
2023-12-07 14:58:27 +0000

    Increase tile size to 512x512 pixels.
    Closes #78

--- scripts/tile.py
+++ scripts/tile.py
@@ -18,7 +18,7 @@ import xarray as xr
 from rasterio.enums import ColorInterp
 
 NODATA = 0
-TILE_SIZE = 256
+TILE_SIZE = 512
 PIXELS_PER_TILE = TILE_SIZE * TILE_SIZE
 BAD_PIXEL_MAX_PERCENTAGE = 0.3
 SCL_FILTER = [0, 1, 3, 8, 9, 10]
