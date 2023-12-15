e259165a9903b8230fe307f144d476ae1f37074d
Wei Ji <23487320+weiji14@users.noreply.github.com>
2023-12-15 14:51:07 +1300

    Callback function to log Masked Autoencoder reconstructions to WandB (#88)
    * :heavy_plus_sign: Add wandb
    
    A CLI and library for interacting with the Weights and Biases API!
    
    * :loud_sound: Log Masked Autoencoder reconstructions to WandB
    
    Created a custom callback function to log visualizations of the input and output images to the Masked Autoencoder. Only showing the RGB bands of Sentinel-2 for now. A sample of 6 image pairs (original + reconstructed, so 12 in total) is uploaded to Weights and Biases.
    
    Example LightningCLI command: `python trainer.py fit --trainer.max_epochs=20 --data.data_path=data/32VLM --trainer.logger=WandbLogger --trainer.logger.project=clay --trainer.logger.save_dir=checkpoints --trainer.callbacks+=LogMAEReconstructedImage`.
    
    * :heavy_plus_sign: Add scikit-image
    
    Image processing in Python!
    
    * :camera_flash: Apply histogram equalization to RGB images
    
    Enhance low contrast images by applying a histogram equalization stretching algorithm on the RGB images, instead of dividing by a magic number like 6000.
    
    * :wrench: Increase default sample size from 6 to 8
    
    More samples to look at! Also only running einsum conversion on as many samples as needed rather than the whole batch, and handling cases where num_samples may be more than the batch_size.
    
    * :technologist: Make wandb a somewhat optional dependency
    
    Allows for `from src.callback_wandb import LogMAEReconstruction` to run, even without wandb being installed. Helpful if someone doesn't want to install wandb for whatever reason.
    
    * :white_check_mark: Add unit test for LogMAEReconstruction
    
    Testing that the LogMAEReconstruction callback works to save a set of images to WandB. Testing this in offline mode only, with checks that artifacts are saved locally, and that the wandb images have the correct caption and format.
    
    * :bug: Compare expected folders using set instead of list
    
    Order of the folders could change, so using set instead of list.
    
    * :test_tube: Prevent WandB logger from saving logs to local drive for now
    
    Setting WANDB_MODE="disabled", so no files are logged to disk, though the wandb.Image(s) are still created. See if this helps to resolve the exit code 255 issue on GitHub Actions.
    
    * :memo: Fix a typo and improve docstring
    
    Minor changes to the docstring of the on_validation_batch_end method, and a typo fix.

---
+++ src/tests/test_callbacks.py
@@ -0,0 +1,66 @@
+"""
+Tests for custom Callback plugins.
+
+Checks to ensure that the hooks in callbacks can be triggered and produce the
+correct output results.
+"""
+import tempfile
+
+import lightning as L
+import pytest
+import torch
+
+from src.callbacks_wandb import LogMAEReconstruction
+from src.model_vit import ViTLitModule
+
+
+# %%
+def test_callbacks_wandb_log_mae_reconstruction():
+    """
+    Ensure that the LogMAEReconstruction callback can log a set of images to
+    Weights & Biases.
+    """
+    wandb = pytest.importorskip(modname="wandb")
+
+    # Run tests in a temporary folder
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        pl_module: L.LightningModule = ViTLitModule()
+        trainer: L.Trainer = L.Trainer(
+            accelerator="auto",
+            callbacks=[LogMAEReconstruction(num_samples=4)],
+            devices=1,
+            logger=L.pytorch.loggers.WandbLogger(mode="disabled", save_dir=tmpdirname),
+            default_root_dir=tmpdirname,
+        )
+        callback: L.Callback = trainer.callbacks[0]
+
+        assert callback.ready is False  # callback state is not ready at first
+        callback.on_sanity_check_end(trainer=trainer, pl_module=pl_module)
+        assert callback.ready is True  # callback state is ready after sanity_check
+
+        wandb_images: list[wandb.Image] = callback.on_validation_batch_end(
+            trainer=trainer,
+            pl_module=pl_module,
+            outputs={"logits": torch.rand(4, 64, 53248)},
+            batch={"image": torch.rand(4, 13, 512, 512)},
+            batch_idx=0,
+        )
+
+        # Check that wandb saved some log files to the temporary directory
+        # assert os.path.exists(path := f"{tmpdirname}/wandb/latest-run/")
+        # assert set(os.listdir(path=path)) == set(
+        #     [
+        #         f"run-{trainer.logger.version}.wandb",
+        #         "tmp",
+        #         "files",
+        #         "logs",
+        #     ]
+        # )
+
+        # Check that images logged by WandB have the correct caption and format
+        assert len(wandb_images) == 8  # noqa: PLR2004
+        assert all(isinstance(w, wandb.Image) for w in wandb_images)
+        assert wandb_images[0]._caption == "RGB Image 0"
+        assert wandb_images[0].format == "png"
+        assert wandb_images[1]._caption == "Reconstructed 0"
+        assert wandb_images[1].format == "png"
