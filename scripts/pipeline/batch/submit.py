08a9e8c2afc32d7039605ef822e93f9b6c7204b2
Daniel Wiesmann <daniel@wiesmann.pt>
2024-03-05 15:11:51 +0000

    Convert batch command to array job

--- scripts/pipeline/batch/submit.py
+++ scripts/pipeline/batch/submit.py
@@ -1,31 +1,34 @@
+import os
+
 import boto3
 
 batch = boto3.client("batch", region_name="us-east-1")
 
-NR_OF_TILES_IN_SAMPLE_FILE = 1517
+NR_OF_TILES_IN_SAMPLE_FILE = 2728
 
-PC_KEY = "***"
+PC_KEY = os.environ["PC_SDK_SUBSCRIPTION_KEY"]
 
-for i in range(NR_OF_TILES_IN_SAMPLE_FILE):
-    job = {
-        "jobName": f"fetch-and-run-{i}",
-        "jobQueue": "fetch-and-run",
-        "jobDefinition": "fetch-and-run",
-        "containerOverrides": {
-            "command": ["datacube.py", "--index", f"{i}", "--bucket", "clay-tiles-02"],
-            "environment": [
-                {"name": "BATCH_FILE_TYPE", "value": "zip"},
-                {
-                    "name": "BATCH_FILE_S3_URL",
-                    "value": "s3://clay-fetch-and-run-packages/batch-fetch-and-run.zip",
-                },
-                {"name": "PC_SDK_SUBSCRIPTION_KEY", "value": f"{PC_KEY}"},
-            ],
-            "resourceRequirements": [
-                {"type": "MEMORY", "value": "8000"},
-                {"type": "VCPU", "value": "4"},
-            ],
-        },
-    }
 
-    print(batch.submit_job(**job))
+job = {
+    "jobName": "fetch-and-run-datacube",
+    "jobQueue": "fetch-and-run",
+    "jobDefinition": "fetch-and-run",
+    "containerOverrides": {
+        "command": ["datacube.py", "--bucket", "clay-tiles-04"],
+        "environment": [
+            {"name": "BATCH_FILE_TYPE", "value": "zip"},
+            {
+                "name": "BATCH_FILE_S3_URL",
+                "value": "s3://clay-fetch-and-run-packages/batch-fetch-and-run.zip",
+            },
+            {"name": "PC_SDK_SUBSCRIPTION_KEY", "value": f"{PC_KEY}"},
+        ],
+        "resourceRequirements": [
+            {"type": "MEMORY", "value": "8000"},
+            {"type": "VCPU", "value": "2"},
+        ],
+    },
+    "arrayProperties": {"size": NR_OF_TILES_IN_SAMPLE_FILE},
+}
+
+print(batch.submit_job(**job))
