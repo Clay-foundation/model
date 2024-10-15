40659da8e673e12bc126ff8d0d4d22b04acc1a31
Daniel Wiesmann <yellowcap@users.noreply.github.com>
2024-10-15 16:00:20 +0100

    Make path relative

--- embeddings/all-sentinel.py
+++ embeddings/all-sentinel.py
@@ -2,7 +2,6 @@ import gzip
 import json
 import logging
 import os
-import sys
 
 import boto3
 from pystac import Item
@@ -13,71 +12,27 @@ from embeddings.utils import load_clay
 logger = logging.getLogger("clay")
 
 
-# Create 2023 file from full archive
-
-s3 = boto3.resource("s3")
-
-
-def log(comment):
-    sys.stdout.write(f"\r{comment}")
-
-
-log_every = 100000
-
-count = 0
-
-# with gzip.open(f"data/element84-tiles-2023.gz", "wt") as dst:
-#     with gzip.open(f"data/element84-tiles.list.gz") as fl:
-#         line = fl.readline()
-#         while line:
-#             line =  line.decode().rstrip()
-#             c = line.split("/")
-#             if c[4] == "2019":
-#                 line = fl.readline()
-#                 continue
-#             elif int(c[7]) < 2023:
-#                 line = fl.readline()
-#                 continue
-#             elif not line.endswith("L2A.json"):
-#                 line = fl.readline()
-#                 continue
-
-#             count += 1
-#             if count % log_every == 0:
-#                 log(f"Found {count} scenes... {line}")
-
-#             dst.write(line + "\n")
-#             line = fl.readline()
-
-
-def open_manifest(path):
-    pass
-
-
-data = open_manifest()
-
-index = 42
-
-key = data[index].decode().rstrip().replace("s3://sentinel-cogs/", "")
-
-stac_json = json.load(s3.Object("sentinel-cogs", key).get()["Body"])
-
-item = Item.from_dict(stac_json)
-
-
-indexer = Sentinel2Indexer(item, chip_max_nodata=0.1)
-
-
 SCENES_LIST = "data/element84-tiles-2023.gz"
 
 
 def open_scenes_list():
     with gzip.open(SCENES_LIST) as fl:
-        return fl.readlines()
+        data = fl.readlines()
+    return [dat.decode().rstrip() for dat in data]
 
 
 def process_scene(clay, path, batchsize):
-    pass
+    key = path.replace("s3://sentinel-cogs/", "")
+
+    s3 = boto3.resource("s3")
+
+    stac_json = json.load(s3.Object("sentinel-cogs", key).get()["Body"])
+
+    item = Item.from_dict(stac_json)
+
+    indexer = Sentinel2Indexer(item, chip_max_nodata=0.1)
+
+    return indexer
 
 
 def process():
